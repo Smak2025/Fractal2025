0d14b14fb44615d2571b8c7acee6a0f9b8893417
Smak2025 <smak-80+2025@yandex.ru>
2025-09-25 10:09:25 +0300

    Java 21

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,14 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
@@ -17,11 +22,23 @@ fun App() {
 
 @Composable
 fun Content(modifier: Modifier = Modifier) {
-    Text("Hello, World!")
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(20.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Button(onClick = {}) {
+            Text("Нажать нежно")
+        }
+        Text("Привет, Мир!")
+    }
 }
 
 fun main(): Unit = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Окно первого приложения"
+    ) {
         App()
     }
 }
