aa8c93b05ff7383280cca9ac6b5de9f2481ef705
Smak2025 <smak-80+2025@yandex.ru>
2025-09-25 09:27:06 +0300

    Initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,15 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
     MaterialTheme {
         Content(Modifier.fillMaxWidth())
     }
@@ -28,22 +17,10 @@ fun App() {
 
 @Composable
 fun Content(modifier: Modifier = Modifier) {
-    var text by remember { mutableStateOf("") }
-    var res by remember { mutableStateOf("")}
-    Column(modifier = modifier) {
-        OutlinedTextField(
-            text,
-            onValueChange = { text = it },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Button(onClick = { res = text}){
-            Text("Нажать нежно")
-        }
-        Text(res)
-    }
+    Text("Hello, World!")
 }
 
-fun main() = application {
+fun main(): Unit = application {
     Window(onCloseRequest = ::exitApplication) {
         App()
     }
